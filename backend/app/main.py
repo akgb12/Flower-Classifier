7397cfbe174db761f9207eb81441dddb01bf43b9
akgb12 <aneykanji@gmail.com>
2026-04-30 00:36:44 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://flower-classifier-nu.vercel.app/",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
